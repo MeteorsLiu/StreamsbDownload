1ff7a981ce515f62ce25b0aef579ccf0f81dd9a2
GuoHaoLan <coopticks@hotmail.com>
2022-12-14 21:01:43 +0800

    P

--- stream/stream_test.go
+++ stream/stream_test.go
@@ -5,7 +5,7 @@ import (
 )
 
 func TestParse(t *testing.T) {
-	r, err := Parse("https://sblongvu.com/e/pynjd03yambo.html")
+	r, err := Parse("https://sblongvu.com/e/olok287zvvqe.html")
 	if err != nil {
 		t.Errorf("%v", err)
 		return
@@ -16,5 +16,5 @@ func TestParse(t *testing.T) {
 		t.Errorf("%v", err)
 		return
 	}
-	r.Download("/home/nfs/py/ttt.mp4")
+	r.Download("/home/nfs/py/yyy.mp4")
 }
