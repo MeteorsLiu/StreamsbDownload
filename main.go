d9bdf6d337c81eb7e935033a80f2f39277bf1a9c
GuoHaoLan <coopticks@hotmail.com>
2022-12-15 17:02:59 +0800

    P

--- main.go
+++ main.go
@@ -24,7 +24,7 @@ func main() {
 	defer f.Close()
 	scanner := bufio.NewScanner(f)
 	for scanner.Scan() {
-		ret := strings.SplitAfterN(scanner.Text(), ":", 2)
+		ret := strings.SplitN(scanner.Text(), ":", 2)
 		fmt.Println(ret[0], ret[1])
 	}
 
